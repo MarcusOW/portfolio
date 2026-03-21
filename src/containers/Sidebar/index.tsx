19b4ec23586b1da8e181e60ca30cf0ca0683f676
Marcus <marcusowlvl1@gmail.com>
2026-03-21 12:25:03 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/MarcusOW.png" />
-    <Titulo fontSize={20}>Marcus Souza</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Marcus Souza</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        marcusow
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
